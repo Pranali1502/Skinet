b99cb7e53cc958de90ed0394ae8650c6e35fb2a5
Pranali1502 <mailto:pani10295@gmail.com>
2024-02-20 13:47:41 +0530

    changes4

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,13 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+
+using Core.Entity;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class ProductsController: ControllerBase
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(StoreContext context) : ControllerBase
     {
-        
+        private readonly StoreContext _storeContext = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products = await _storeContext.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _storeContext.Products.FindAsync(id);
+        }
     }
 }
